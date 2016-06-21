26d8cc8d327a4732f9df3719f0a7f7579161581f
Artur <abdurahmanov.art@gmail.com>
2016-06-21 11:40:16 +0300

    details

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,23 @@
 "use strict";
 
 var gulp = require("gulp");
-var plumber = require("gulp-plumber");
 var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync");
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
     .pipe(plumber())
-    .pipe(less([
+    .pipe(less())
+    .pipe(postcss([
       autoprefixer({browsers: [
-        "last 1 version",
-        "last 2 Chrome versions",
-        "last 2 Firefox versions",
-        "last 2 Opera versions",
-        "last 2 Edge versions"
+        "last 3 version",
+        "last 4 Chrome versions",
+        "last 4 Firefox versions",
+        "last 4 Opera versions",
+        "last 4 Edge versions"
       ]})
     ]))
     .pipe(gulp.dest("css"))
@@ -32,5 +34,5 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
-  gulp.watch("*.js").on("change", server.reload);
+  gulp.watch("js/*.js").on("change", server.reload);
 });
